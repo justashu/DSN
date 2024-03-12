25cb9effbc757756a7ace0e1a9a4bc9d006c6733
Ashutosh Yadav <86598413+justashu@users.noreply.github.com>
2024-03-12 11:04:22 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,10 +1,9 @@
 import random
 import os
-import torch.backends.cudnn as cudnn
+import torch
 import torch.optim as optim
 import torch.utils.data
 import numpy as np
-from torch.autograd import Variable
 from torchvision import datasets
 from torchvision import transforms
 from model_compat import DSN
@@ -19,8 +18,9 @@ from test import test
 source_image_root = os.path.join('.', 'dataset', 'mnist')
 target_image_root = os.path.join('.', 'dataset', 'mnist_m')
 model_root = 'model'
-cuda = True
-cudnn.benchmark = True
+cuda = torch.cuda.is_available()
+if cuda:
+    cudnn.benchmark = True
 lr = 1e-2
 batch_size = 32
 image_size = 28
@@ -86,22 +86,8 @@ my_net = DSN()
 # setup optimizer   #
 #####################
 
-
-def exp_lr_scheduler(optimizer, step, init_lr=lr, lr_decay_step=lr_decay_step, step_decay_weight=step_decay_weight):
-
-    # Decay learning rate by a factor of step_decay_weight every lr_decay_step
-    current_lr = init_lr * (step_decay_weight ** (step / lr_decay_step))
-
-    if step % lr_decay_step == 0:
-        print 'learning rate is set to %f' % current_lr
-
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = current_lr
-
-    return optimizer
-
-
 optimizer = optim.SGD(my_net.parameters(), lr=lr, momentum=momentum, weight_decay=weight_decay)
+scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=step_decay_weight)
 
 loss_classification = torch.nn.CrossEntropyLoss()
 loss_recon1 = MSE()
@@ -124,12 +110,11 @@ for p in my_net.parameters():
 # training network          #
 #############################
 
-
 len_dataloader = min(len(dataloader_source), len(dataloader_target))
 dann_epoch = np.floor(active_domain_loss_step / len_dataloader * 1.0)
 
 current_step = 0
-for epoch in xrange(n_epoch):
+for epoch in range(n_epoch):
 
     data_source_iter = iter(dataloader_source)
     data_target_iter = iter(dataloader_target)
@@ -142,7 +127,7 @@ for epoch in xrange(n_epoch):
         # target data training            #
         ###################################
 
-        data_target = data_target_iter.next()
+        data_target = next(data_target_iter)
         t_img, t_label = data_target
 
         my_net.zero_grad()
@@ -163,9 +148,9 @@ for epoch in xrange(n_epoch):
 
         input_img.resize_as_(t_img).copy_(t_img)
         class_label.resize_as_(t_label).copy_(t_label)
-        target_inputv_img = Variable(input_img)
-        target_classv_label = Variable(class_label)
-        target_domainv_label = Variable(domain_label)
+        target_inputv_img = torch.tensor(input_img, requires_grad=True)
+        target_classv_label = torch.tensor(class_label)
+        target_domainv_label = torch.tensor(domain_label)
 
         if current_step > active_domain_loss_step:
             p = float(i + (epoch - dann_epoch) * len_dataloader / (n_epoch - dann_epoch) / len_dataloader)
@@ -177,7 +162,7 @@ for epoch in xrange(n_epoch):
             target_dann = gamma_weight * loss_similarity(target_domain_label, target_domainv_label)
             loss += target_dann
         else:
-            target_dann = Variable(torch.zeros(1).float().cuda())
+            target_dann = torch.zeros(1).float().cuda()
             result = my_net(input_data=target_inputv_img, mode='target', rec_scheme='all')
             target_privte_code, target_share_code, _, target_rec_code = result
 
@@ -191,11 +176,11 @@ for epoch in xrange(n_epoch):
         loss.backward()
         optimizer.step()
 
-        #############################################
-        # source data training            #  #
-        #############################################
+        ###################################
+        # source data training            #
+        ###################################
 
-        data_source = data_source_iter.next()
+        data_source = next(data_source_iter)
         s_img, s_label = data_source
 
         my_net.zero_grad()
@@ -217,9 +202,9 @@ for epoch in xrange(n_epoch):
 
         input_img.resize_as_(input_img).copy_(s_img)
         class_label.resize_as_(s_label).copy_(s_label)
-        source_inputv_img = Variable(input_img)
-        source_classv_label = Variable(class_label)
-        source_domainv_label = Variable(domain_label)
+        source_inputv_img = torch.tensor(input_img, requires_grad=True)
+        source_classv_label = torch.tensor(class_label)
+        source_domainv_label = torch.tensor(domain_label)
 
         if current_step > active_domain_loss_step:
 
@@ -230,41 +215,36 @@ for epoch in xrange(n_epoch):
             source_dann = gamma_weight * loss_similarity(source_domain_label, source_domainv_label)
             loss += source_dann
         else:
-            source_dann = Variable(torch.zeros(1).float().cuda())
+            source_dann = torch.zeros(1).float().cuda()
             result = my_net(input_data=source_inputv_img, mode='source', rec_scheme='all')
             source_privte_code, source_share_code, _, source_class_label, source_rec_code = result
 
-        # source_classification = loss_classification(source_class_label, source_classv_label)  ### Classification error
-        # loss += source_classification                                                         ###  REMOVED
+        # source_classification = loss_classification(source_class_label, source_classv_label)
+        # loss += source_classification
 
-        # source_diff = beta_weight * loss_diff(source_privte_code, source_share_code)          ### L_difference (Source Enc and Shared Enc) REMOVED
+        # source_diff = beta_weight * loss_diff(source_privte_code, source_share_code)
         # loss += source_diff
-        # source_mse = alpha_weight * loss_recon1(source_rec_code, source_inputv_img) 
+        # source_mse = alpha_weight * loss_recon1(source_rec_code, source_inputv_img)
         # loss += source_mse
         # source_simse = alpha_weight * loss_recon2(source_rec_code, source_inputv_img)
-        # loss += source_simse                                                                  ### L_reconstructin REMOVED
+        # loss += source_simse
 
         loss.backward()
-        optimizer = exp_lr_scheduler(optimizer=optimizer, step=current_step)
+        scheduler.step()
         optimizer.step()
 
         i += 1
         current_step += 1
-    print 'source_classification: %f, source_dann: %f, source_diff: %f, ' \
+    print('source_classification: %f, source_dann: %f, source_diff: %f, ' \
           'source_mse: %f, source_simse: %f, target_dann: %f, target_diff: %f, ' \
           'target_mse: %f, target_simse: %f' \
-          % (source_classification.data.cpu().numpy(), source_dann.data.cpu().numpy(), source_diff.data.cpu().numpy(),
-             source_mse.data.cpu().numpy(), source_simse.data.cpu().numpy(), target_dann.data.cpu().numpy(),
-             target_diff.data.cpu().numpy(),target_mse.data.cpu().numpy(), target_simse.data.cpu().numpy())
+          % (source_classification.detach().cpu().numpy(), source_dann.detach().cpu().numpy(), source_diff.detach().cpu().numpy(),
+             source_mse.detach().cpu().numpy(), source_simse.detach().cpu().numpy(), target_dann.detach().cpu().numpy(),
+             target_diff.detach().cpu().numpy(),target_mse.detach().cpu().numpy(), target_simse.detach().cpu().numpy()))
 
     # print 'step: %d, loss: %f' % (current_step, loss.cpu().data.numpy())
     torch.save(my_net.state_dict(), model_root + '/dsn_mnist_mnistm_epoch_' + str(epoch) + '.pth')
     test(epoch=epoch, name='mnist')
     test(epoch=epoch, name='mnist_m')
 
-print 'done'
-
-
-
-
-
+print('done')
